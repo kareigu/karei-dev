b6310134262f18271f8975688343cb63509c4260
mxr <eetu.kivelae@gmail.com>
2021-07-05 17:42:14 +0300

    Styling: Added pattern background and updated home

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -33,13 +33,13 @@ impl Component for Home {
     html! {
       <div class="flex flex-col justify-center items-center mt-10">
         <div class="w-52 h-52 rounded-full bg-logo bg-cover mt-56" >
-          <div class="w-52 h-52 rounded-full border-secondary-accent-lt border-2 sm:animate-ping select-none pointer-events-none">
-            <div class="w-52 h-52 rounded-full border-tertiary-accent-md border-2 animate-pulse sm:animate-ping select-none pointer-events-none"/>
+          <div class="w-52 h-52 rounded-full border-tertiary-accent-lt border-2 select-none pointer-events-none">
+            <div class="w-52 h-52 rounded-full border-secondary-accent-md border-2 animate-pulse sm:animate-ping select-none pointer-events-none"/>
           </div>
         </div>
 
         
-        <div class="flex flex-col justify-center items-center mb-10 mt-56 bg-black bg-opacity-20 pt-5 px-14 rounded-md hover:border-2 hover:border-base-lt">
+        <div class="flex flex-col justify-center items-center mt-56 bg-black bg-opacity-20 mb-20 pt-5 px-14 rounded-md hover:border-2 hover:border-base-lt">
           <Link<AppRoutes> route=AppRoutes::Forked classes=classes!("flex flex-col justify-center items-center".to_string())>
             <h1 class="font-mulish lg:text-4xl md:text-3xl select-none">{"Forked YouTube Gaming"}</h1>
             <img 
