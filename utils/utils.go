724072f8c06fa46f201c640c73723432006f5b7d
karei <31644842+kareigu@users.noreply.github.com>
2022-05-12 19:39:56 +0300

    Update utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -14,39 +14,39 @@ func init() {
 	PageData = make(map[string]string)
 
 	PageData["/"] = `
-		<meta property="og:title" content="mxrr.dev" />
+		<meta property="og:title" content="karei.dev" />
 		<meta property="og:description" content="väsinud" />
 		<meta property="og:type" content="website" />
-		<meta property="og:url" content="https://mxrr.dev/" />
-		<meta property="og:image" content="https://mxrr.dev/static/logo.gif" />`
+		<meta property="og:url" content="https://karei.dev/" />
+		<meta property="og:image" content="https://karei.dev/static/logo.gif" />`
 
 	PageData["/forkedytg"] = `
 		<meta property="og:title" content="Forked YouTube Gaming" />
 		<meta property="og:description" content="ForkedYTG enhances YouTube livestreams with more emotes (Twitch, BTTV Emotes), new features, and more." />
 		<meta property="og:type" content="website" />
-		<meta property="og:url" content="https://mxrr.dev/forkedytg" />
-		<meta property="og:image" content="https://raw.githubusercontent.com/mxrr/BetterYTG/master/src/assets/icons/BetterYTG_red_128.png" />`
+		<meta property="og:url" content="https://karei.dev/forkedytg" />
+		<meta property="og:image" content="https://raw.githubusercontent.com/kareigu/BetterYTG/master/src/assets/icons/BetterYTG_red_128.png" />`
 
 	PageData["/projects"] = `
-		<meta property="og:title" content="mxrr.dev - projects" />
+		<meta property="og:title" content="karei.dev - projects" />
 		<meta property="og:description" content="Various projects" />
 		<meta property="og:type" content="website" />
-		<meta property="og:url" content="https://mxrr.dev/projects" />
-		<meta property="og:image" content="https://mxrr.dev/static/logo.gif" />`
+		<meta property="og:url" content="https://karei.dev/projects" />
+		<meta property="og:image" content="https://karei.dev/static/logo.gif" />`
 
 	PageData["/about"] = `
-		<meta property="og:title" content="mxrr.dev - about" />
+		<meta property="og:title" content="karei.dev - about" />
 		<meta property="og:description" content="About page" />
 		<meta property="og:type" content="website" />
-		<meta property="og:url" content="https://mxrr.dev/about" />
-		<meta property="og:image" content="https://mxrr.dev/static/logo.gif" />`
+		<meta property="og:url" content="https://karei.dev/about" />
+		<meta property="og:image" content="https://karei.dev/static/logo.gif" />`
 
 	PageData["404"] = `
-		<meta property="og:title" content="mxrr.dev - 404" />
+		<meta property="og:title" content="karei.dev - 404" />
 		<meta property="og:description" content="Mis se vika on" />
 		<meta property="og:type" content="website" />
-		<meta property="og:url" content="https://mxrr.dev/" />
-		<meta property="og:image" content="https://mxrr.dev/static/PepegaSit.png" />`
+		<meta property="og:url" content="https://karei.dev/" />
+		<meta property="og:image" content="https://karei.dev/static/PepegaSit.png" />`
 
 }
 
